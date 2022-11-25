f28a981a984ad71b0d1213c30752000bac60b0b9
iamchacha021 <iamchacha021@gmail.com>
2022-11-25 23:47:00 +0300

    modified my random number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const num1 = 31;
 const num2 = 2;
 let multiply = num1 * num2;
 
-const random = Math.ceil(Math.random() * 10) - 1;
+const random = Math.floor(Math.random() * 10) + 1;
 
 const num3 = 49;
 const num4 = 5;
